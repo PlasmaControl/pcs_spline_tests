71696c8180a3428433d406b81250f4b9f74fd2fc
Joe Abbate <joseph.abbate7@gmail.com>
2022-04-29 16:01:22 -0700

    more clear plotting for the test python script

--- run_spline.py
+++ run_spline.py
@@ -15,6 +15,7 @@ shot=936935 #936935, 187076
 time=629
 
 ftssmhat_data=[]
+ftssrot_data=[]
 times=[]
 ftsspsin_data=[]
 ftssn_data=toksearch.PtDataSignal('ftssn').fetch(shot)['data']
@@ -23,9 +24,13 @@ for i in range(1,76):
     ftssmhat_data.append(sig['data'])
     sig=toksearch.PtDataSignal(f'ftsspsin{i}').fetch(shot)
     ftsspsin_data.append(sig['data'])
+    sig=toksearch.PtDataSignal(f'ftssrot{i}').fetch(shot)
+    ftssrot_data.append(sig['data'])
+
 times=sig['times']
 ftssmhat_data=np.array(ftssmhat_data)
 ftsspsin_data=np.array(ftsspsin_data)
+ftssrot_data=np.array(ftssrot_data)
 
 # 0 index here is just because nonzero returns a tuple of arrays (squeeze)
 nonzero_time_inds=np.nonzero(ftssn_data)[0]
@@ -36,22 +41,25 @@ time_ind=np.argmin(np.abs(times-nearest_available_time))
 num_cer_points=int(ftssn_data[time_ind])
 psinspline=ftsspsin_data[:num_cer_points,time_ind]
 mhatspline=ftssmhat_data[:num_cer_points,time_ind]
+srot=ftssrot_data[:num_cer_points,time_ind]
 
 # ftx signals only available in recent PCS versions
-if shot>190000:
+if False: #shot>190000:
     ftxpr=toksearch.PtDataSignal('ftxpr').fetch(shot)
     ftxpr['data']=ftxpr['data'].reshape((-1,121))
     ftxpr['times']=ftxpr['times'][:ftxpr['data'].shape[0]]
 
     ftx_time_ind=np.argmin(np.abs(ftxpr['times']-nearest_available_time))
-    plt.plot(np.linspace(0,1.2,NFIT),ftxpr['data'][ftx_time_ind,:],label='online version')
+    plt.plot(np.linspace(0,1.2,NFIT),ftxpr['data'][ftx_time_ind,:],label='online fit',c='k')
 
 v=spline_eval(psinspline,mhatspline,num_cer_points,NFIT)
-plt.plot(np.linspace(0,1.2,NFIT),v,label='offline version',linestyle='--')
-
+plt.scatter(psinspline,
+            srot,
+            label='online data',c='k')
 plt.scatter(psinspline,
             mhatspline,
-            marker='x',label='data to fit',c='k')
+            marker='x',label='online mhat',c='k')
+plt.plot(np.linspace(0,1.2,NFIT),v,label='online fit',c='k')
 
 try:
     import h5py
@@ -64,9 +72,9 @@ try:
         mPsi=mPsi[:-1] # still not sure why the last index is always 0, should talk to ricardo (TODO)
         mHat=mHat[:-1]
         splined_rot=spline_eval(mPsi,mHat,len(mHat))
-        plt.scatter(psi, rot, label='raw rotation')
-        plt.scatter(mPsi, mHat, label='mHat rotation')
-        plt.plot(np.linspace(0,1.2,NFIT), splined_rot, label='splined rotation')
+        plt.scatter(psi, rot, label='offline data',c='r')
+        plt.scatter(mPsi, mHat, label='offline mhat', marker='x',c='r')
+        plt.plot(np.linspace(0,1.2,NFIT), splined_rot, label='offline fit',c='r')
 except:
     print("Use tmp.yaml for new_database_maker.py from PlasmaControl/data-fetching repo to read in raw CER rotation from the base shot associated with your sim")
 plt.title(f'Rotation fits at {nearest_available_time:.0f} ms, shot {shot}')
